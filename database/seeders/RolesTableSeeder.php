d1ab6547583523096eabecaf60bd6fdd727637b4
Abajja Mohammed <mohamedabajja3@gmail.com>
2026-01-05 14:43:11 +0100

    fixed some conflit errors

--- database/seeders/RolesTableSeeder.php
+++ database/seeders/RolesTableSeeder.php
@@ -3,69 +3,34 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
-use Illuminate\Support\Facades\DB;
 use App\Models\Role;
-
-//edited by mohammed 05/01
 
 class RolesTableSeeder extends Seeder
 {
-    /**
-     * Run the database seeds.
-     */
     public function run(): void
     {
-<<<<<<< Updated upstream
-        $roles = [
-            'invité',
-            'utilisateur_interne',
-            'responsable_technique',
-            'admin',
-        ];
-=======
-    
-        /**
-     *   $roles = [
-     *       'Guest',
-     *       'Internal User',
-     *       'Technical Resource Manager',
-     *       'Data Center Administrator',
-     *   ];
-*
-     *   foreach ($roles as $role) {
-     *       DB::table('roles')->insert([
-     *           'name' => $role,
-     *           'created_at' => now(),
-     *           'updated_at' => now(),
-     *       ]);
-      *  }
-            */
-
-        //mohammed: the code u did misses description and uses wrong roles names 
-         // (Guest)
+        // 1. Invité
         Role::create([
             'name' => 'invite', 
             'description' => 'Visiteur externe (Lecture seule et demande de compte)'
         ]);
 
-        // (Internal User)
+        // 2. Utilisateur Interne
         Role::create([
             'name' => 'utilisateur_interne', 
             'description' => 'Personnel interne (Ingénieur, Enseignant, Doctorant)'
         ]);
 
-        // (Technical Manager)
+        // 3. Responsable Technique
         Role::create([
             'name' => 'responsable_technique', 
             'description' => 'Gestionnaire responsable d\'un ensemble de ressources'
         ]);
 
-        // (Admin)
+        // 4. Administrateur
         Role::create([
             'name' => 'admin', 
             'description' => 'Super-Administrateur avec tous les droits'
         ]);
->>>>>>> Stashed changes
-
     }
 }
